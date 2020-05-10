99c92236406fc5288b0e14644a1d034633d98c96
Dictor <kimdictor@gmail.com>
2020-05-10 04:35:45 +0000

    Enhance swagger

--- route.go
+++ route.go
@@ -6,33 +6,33 @@ import (
 	"net/http"
 )
 
+type KeyRequestForm struct {
+	Key string `json:"key"`
+}
+
+type TokenResponseForm struct {
+	Token string `json:"token"`
+}
+
 // @summary API 자격증명 토큰을 발급합니다
 // @description 액세스 키를 제출받고, 유효한 액세스키라면 API 자격증명 토큰을 발급합니다
 // @id issue-token
 // @accept json
 // @produce json
-// @param key body string true "액세스 키"
-// @success 200 {body} string "API 자격증명 토큰"
-// @failure 400
-// @failure 401
+// @param key body KeyRequestForm true "액세스 키"
+// @success 200 {object} TokenResponseForm "유효한 액세스 키를 제출했을때 API 자격증명 토큰이 반환됩니다."
+// @failure 400 "요청이 정해진 형식에 부합하지 않습니다."
+// @failure 401 "액세스 키가 유효하지 않습니다."
 // @router /token [POST]
 func rIssueToken(c echo.Context) error {
-	req := map[string]interface{}{}
+	req := KeyRequestForm{}
 	if err := c.Bind(&req); err != nil {
 		c.Logger().Error(err)
 		return c.NoContent(http.StatusBadRequest)
 	}
 
-	var reqkey string
-	if val, res := req["key"].(string); !res {
-		c.Logger().Error("Key retrieve fail")
-		return c.NoContent(http.StatusBadRequest)
-	} else {
-		reqkey = val
-	}
-
-	if searchSlice(reqkey, &validAccessKey) {
-		return c.JSON(http.StatusOK, map[string]string{"token": newToken().Token})
+	if searchSlice(req.Key, &validAccessKey) {
+		return c.JSON(http.StatusOK, TokenResponseForm{newToken().Token})
 	} else {
 		return c.NoContent(http.StatusUnauthorized)
 	}
@@ -46,9 +46,9 @@ func rIssueToken(c echo.Context) error {
 // @securityDefinitions.apikey API-TOKEN
 // @param API-TOKEN header string true "API 자격증명 코드"
 // @param info body CalculateRequest true "배송자와 배송 물품 정보"
-// @success 200 {object} CalculateResult "배송 경로"
-// @failure 400
-// @failure 401
+// @success 200 {object} CalculateResult "요청에 대한 배송 경로가 반환됩니다."
+// @failure 400 "요청이 정해진 형식에 부합히지 않습니다."
+// @failure 401 "액세스 키가 유효하지 않습니다."
 // @router /path [POST]
 func rCalculatePath(c echo.Context) error {
 	if tok := c.Request().Header.Get("API-TOKEN"); tok == "" {
