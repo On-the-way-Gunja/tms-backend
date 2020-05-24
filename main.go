e9082e1a0c410cf3961ac4a67fc9eb42caa4b91f
Dictor <kimdictor@gmail.com>
2020-05-24 07:07:06 +0000

    Add more allowheader

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main() {
 		Skipper:      middleware.DefaultSkipper,
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
-		AllowHeaders: []string{"API-TOKEN", "Content-Type"},
+		AllowHeaders: []string{"API-TOKEN", echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 	}))
 
 	//Read config
